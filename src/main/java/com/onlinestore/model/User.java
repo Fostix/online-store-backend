53325f5d33c7d874aa9a21fb6f987b52a12d4d99
albert <accretiondiskthat@gmail.com>
2023-04-08 11:50:38 +0500

    add new user method

--- src/main/java/com/onlinestore/model/User.java
+++ src/main/java/com/onlinestore/model/User.java
@@ -8,16 +8,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-//	@GeneratedValue(strategy=GenerationType.UUID)
-//	@GeneratedValue(strategy=GenerationType.AUTO.UUID)
 	@Id
 	@GeneratedValue(generator = "uuid2")
 	@GenericGenerator(name = "uuid2", strategy = "uuid2")
