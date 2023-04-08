937d164bcf237f58dc0089d98e00150e9af9aa1a
albert <accretiondiskthat@gmail.com>
2023-04-08 10:07:26 +0500

    add auto create table if doesn't exist

--- src/main/java/com/onlinestore/model/User.java
+++ src/main/java/com/onlinestore/model/User.java
@@ -2,6 +2,8 @@ package com.onlinestore.model;
 
 import java.util.UUID;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,8 +16,11 @@ import lombok.NonNull;
 @Data
 @Builder
 public class User {
+//	@GeneratedValue(strategy=GenerationType.UUID)
+//	@GeneratedValue(strategy=GenerationType.AUTO.UUID)
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO.UUID)
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "uuid2")
 	private UUID id;
 	@NonNull
 	private String name;
